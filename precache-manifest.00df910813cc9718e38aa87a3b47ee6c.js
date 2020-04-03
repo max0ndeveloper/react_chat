b5fbb66cf8635d2efa927b4da2fdcf1878330b82
Max <maxim2003s@gmail.com>
2020-04-03 16:29:47 +0300

    Updates

---
+++ precache-manifest.00df910813cc9718e38aa87a3b47ee6c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "499d2fea0ece7c82effd0111a342fc2f",
+    "url": "/react_chat/index.html"
+  },
+  {
+    "revision": "5f1ca14309e59476c7e0",
+    "url": "/react_chat/static/css/main.42ef7a49.chunk.css"
+  },
+  {
+    "revision": "be67407f767b46963dea",
+    "url": "/react_chat/static/js/2.8f361350.chunk.js"
+  },
+  {
+    "revision": "a7859b41c23bed46d2a2cd4146c86afb",
+    "url": "/react_chat/static/js/2.8f361350.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f1ca14309e59476c7e0",
+    "url": "/react_chat/static/js/main.02cd2fd6.chunk.js"
+  },
+  {
+    "revision": "9a017b6022c6249955fe",
+    "url": "/react_chat/static/js/runtime-main.270df324.js"
+  }
+]);
